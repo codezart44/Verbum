1be13314eef652194077d0be8f8c116979eaa62e
oskarew <oswa.public@gmail.com>
2025-08-10 19:42:33 +0100

    multiple entry manipulation, INSERT, DELETE, UPDATE and SELECT added

--- src/database/entries.py
+++ src/database/entries.py
@@ -17,7 +17,21 @@ def insert_entry(
     """
     c.execute(sql, parameters)
     return c.rowcount
-    
+
+
+def insert_entries(
+        c : sqlite3.Cursor,
+        entries : list[tuple[str, str, str]]
+) -> int:
+    parameters = [(get_entry_id(t[0]), t[0], t[1], t[2]) for t in entries]
+
+    sql = """--sql
+    INSERT OR IGNORE INTO en_entries ([entry_id], [word], [description], [translation])
+    VALUES (?, ?, ?, ?);
+    """
+    c.executemany(sql, parameters)
+    return c.rowcount
+
 
 def delete_entry(
         c : sqlite3.Cursor,
@@ -30,6 +44,19 @@ def delete_entry(
     DELETE FROM en_entries WHERE [entry_id] = ? ;
     """
     c.execute(sql, parameters)
+    return c.rowcount
+
+
+def delete_entries(
+        c : sqlite3.Cursor,
+        words : list[str],
+) -> int:
+    parameters = [(get_entry_id(w),) for w in words]
+
+    sql = """--sql
+    DELETE FROM en_entries WHERE [entry_id] = ?;
+    """
+    c.executemany(sql, parameters)
     return c.rowcount
 
 
@@ -51,6 +78,21 @@ def update_entry(
     return c.rowcount
 
 
+def update_entries(
+        c : sqlite3.Cursor,
+        entries : list[tuple[str, str, str]]
+) -> int:
+    parameters = [(t[1], t[2], get_entry_id(t[0])) for t in entries]
+
+    sql = """--sql
+    UPDATE en_entries
+    SET [description] = ?, [translation] = ?
+    WHERE [entry_id] = ?;
+    """
+    c.executemany(sql, parameters)
+    return c.rowcount
+
+
 def select_entry(
         c : sqlite3.Cursor,
         word : str,
@@ -66,7 +108,7 @@ def select_entry(
     return c.fetchone()
 
 
-def select_all_entries(
+def select_entries(
         c : sqlite3.Cursor,
 ) -> list[tuple[str]]:
     sql = """--sql
@@ -80,19 +122,23 @@ def select_all_entries(
 def main():
     with get_connection() as conn:
         c = conn.cursor()
-        success1 = insert_entry(c, "test", "this is a test", "test") > 0
-        success2 = update_entry(c, "test", "this is the new desc.", 'tst') > 0
-        # insert_entry(c, "testt", "this is a test", "test")
-        # success3 = delete_entry(c, "test") > 0
 
-        one = select_entry(c, "test")
-        _all = select_all_entries(c)
+        entries = [
+            ("monkey", "hairy man", "apa"),
+            ("rabbit", "funny furry thing", "kanin"),
+            ("turtle", "u wont crack 'im", "sköldpadda"),
+            ("lion", "big pussy", "lejon"),
+            ("skbhf", "nonsense", None),
+        ]
+        words = ["monkey", "rabbit", "turtle", "lion"]
 
-        print(success1)
-        print(success2)
+        # success = insert_entries(c, entries) > 0
+        # success = delete_entries(c, words) > 0
+        success = update_entries(c, entries) > 0
 
-        print(one)
-        print(_all)
+        print(success)
+
+        
     pass
 
 if __name__=="__main__":
--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from typing import Literal
 
 def main():
-    
+
     pass
     
 if __name__=="__main__":
     main()
+    
